ee51aa4f193cb83ee60c8fd177c545cc8b6878fa
Jacmatelm91 <dm66hyczkq@privaterelay.appleid.com>
2026-02-01 06:14:13 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jacob-list-v13';
+const CACHE_NAME = 'jacob-list-v13.1';
 const ASSETS = [
   './',
   './index.html',
@@ -6,20 +6,45 @@ const ASSETS = [
   'https://unpkg.com/html5-qrcode'
 ];
 
-// Install event: Cache files
+// Install: Cache files immediately
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
     })
   );
+  self.skipWaiting(); // Force new service worker to take over immediately
 });
 
-// Fetch event: Serve from cache if offline
+// Activate: Clean up old caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim(); // Take control of all clients immediately
+});
+
+// Fetch: NETWORK FIRST, then Cache (The Fix)
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    fetch(e.request)
+      .then((res) => {
+        // If we get a good response from internet, save it to cache and return it
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch(() => {
+        // If internet fails, return the cached version
+        return caches.match(e.request);
+      })
   );
 });
